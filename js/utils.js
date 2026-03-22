6dee0861ef1a82bd41932326096497b9aa391d6e
unknown <alvessilvaedson125@gmail.com>
2026-03-21 22:17:14 -0300

    feat(ui): add global toast function

---
+++ js/utils.js
@@ -0,0 +1,16 @@
+function showToast(message, type = "success") {
+
+  const container = document.getElementById("toastContainer");
+  if (!container) return;
+
+  const toast = document.createElement("div");
+  toast.className = `toast ${type}`;
+  toast.innerText = message;
+
+  container.appendChild(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, 3000);
+
+}
